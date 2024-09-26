91d10101082aeb0ab7bdc3fb31fb4589356f51ae
Link1515 <h26281660@gmail.com>
2024-09-26 11:16:59 +0800

    create ClockCard component

---
+++ src/components/ClockCard.vue
@@ -0,0 +1,25 @@
+<script setup>
+const props = defineProps({
+  time: {
+    type: String,
+    required: true
+  },
+  timezone: {
+    type: String,
+    required: true
+  }
+});
+</script>
+
+<template>
+  <div class="card">
+    <div class="card-content">
+      <div class="content">
+        <p class="title has-text-centered mb-2">{{ props.time }}</p>
+        <div class="subtitle has-text-centered">
+          {{ props.timezone }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
