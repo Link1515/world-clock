d06d0465873c35041b4116502955c64063aca93e
Link1515 <h26281660@gmail.com>
2024-10-08 16:30:00 +0800

    create modal component

---
+++ src/components/Modal.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { ref, watch } from 'vue';
+
+const isOpen = defineModel({ type: Boolean });
+const modal = ref();
+
+const openModal = () => {
+  modal.value.classList.add('is-active');
+};
+
+const closeModal = () => {
+  modal.value.classList.remove('is-active');
+};
+
+watch(isOpen, () => {
+  isOpen.value ? openModal() : closeModal();
+});
+</script>
+
+<template>
+  <div ref="modal" class="modal">
+    <div class="modal-background" @click="isOpen = false"></div>
+    <div class="modal-content px-4">
+      <div class="box">
+        <slot />
+      </div>
+    </div>
+    <button class="modal-close is-large" @click="isOpen = false"></button>
+  </div>
+</template>
+
+<style scoped>
+.modal-content {
+  max-width: 450px;
+}
+</style>
