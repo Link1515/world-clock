141f70099d2f13684da006684b5b0eb6108fe445
Link1515 <h26281660@gmail.com>
2024-10-15 14:31:53 +0800

    fix share image path by vite config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,30 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
+const projectName = 'world-clock';
+
 export default defineConfig(({ mode }) => {
-  const base = mode === 'production' ? '/world-clock/' : '/';
+  const base = mode === 'production' ? `/${projectName}/` : '/';
 
   return {
     base,
-    plugins: [vue()],
+    plugins: [
+      vue(),
+      {
+        name: 'html-transform',
+        transformIndexHtml: {
+          order: 'pre',
+          handler(html) {
+            if (mode !== 'production') return html;
+
+            return html.replace(
+              /<meta property="og:image" content="(.*?)" \/>/,
+              `<meta property="og:image" content="/${projectName}$1" />`
+            );
+          }
+        }
+      }
+    ],
     resolve: {
       alias: {
         '~': '/src'
