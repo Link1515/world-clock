49029ae552a112a2b86a35c5a9cb8f8816e0c43d
Link1515 <h26281660@gmail.com>
2024-12-11 14:08:01 +0800

    remove redundant code

--- src/services/timeService.js
+++ src/services/timeService.js
@@ -5,9 +5,6 @@ export const getCurrentTime = (
   timezone,
   hourDisplay = HOUR_DISPLAY.HOUR_24
 ) => {
-  if (!timezone) {
-    timezone = getUserTimezone();
-  }
   const format =
     hourDisplay === HOUR_DISPLAY.HOUR_24 ? 'HH:mm:ss' : 'h:mm:ss a';
   return DateTime.now().setZone(timezone).toFormat(format);
